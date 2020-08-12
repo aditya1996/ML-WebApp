15c0a36a6294c31090ecbb6e8e028ef8a2244cd6
Aditya Tiwari <adityatiwari@Adityas-MacBook-Pro.local>
2020-08-12 20:30:30 +0530

    ML-WebApp

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app
+from flaskblog import app, db, bcrypt
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User
+from flask_login import login_user, current_user, logout_user, login_required
 import pandas as pd
 import pymongo
 from pymongo import MongoClient
@@ -14,7 +17,51 @@ from sklearn.metrics import r2_score
 def home():
     return render_template('home.html')
 
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account has been successfully created for {form.username.data}. Try Loggin in Now!','success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+
+        else:
+            flash('Login Unsuccessful! Please check email and password')
+  
+    return render_template('login.html',title = 'Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
+
 @app.route("/database")
+@login_required
 def database():
     # Connecting to Mongo DB
     cluster = MongoClient("mongodb+srv://admin:adminrocks@db01.i7iwq.gcp.mongodb.net/datasets?retryWrites=true&w=majority")
@@ -24,7 +71,9 @@ def database():
 
     return render_template('database.html', ans=ans)
 
+
 @app.route("/loaddb")
+@login_required
 def loaddb():
     # Converting CSV file to pandas dataframe
     df = pd.read_csv('dataset.csv')
@@ -46,7 +95,9 @@ def loaddb():
 
     return render_template('database.html', ans=ans)
 
+
 @app.route("/deletedb")
+@login_required
 def deletedb():
     # Connecting to Mongo DB
     cluster = MongoClient("mongodb+srv://admin:adminrocks@db01.i7iwq.gcp.mongodb.net/datasets?retryWrites=true&w=majority")
@@ -58,12 +109,15 @@ def deletedb():
 
     return render_template('database.html', ans=ans)
 
+
 @app.route("/mlinfo")
 def mlinfo():
 
     return render_template('mlinfo.html')
 
+
 @app.route("/applyml")
+@login_required
 def applyml():
 
     try:
